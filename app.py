a3a623e885207cee23dfaef27f0eaa3648b1398e
Lam Trang <lamtrang@abedu.vn>
2025-05-06 11:00:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,4 +22,4 @@ def crawler_cycle():
 #     threading.Thread(target=crawler_cycle).start()
 #     app.run(host="0.0.0.0", port=9000)
 if __name__ == "__main__":
-    crawler_cycle()
+    crawl_xvideos()
